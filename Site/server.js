632b70215dcb023b931c3418a41c74bfbf2e83df
Bhaikko <siddhartha.pawar17@gmail.com>
2019-08-22 19:05:02 +0530

    Landing page inital commit

--- Site/server.js
+++ Site/server.js
@@ -5,5 +5,10 @@ const server = express();
 server.use(express.json());
 server.use(express.urlencoded({ extended: true}));
 
+server.use(express.static("./public"));
+server.use("/uploads", express.static("./uploads"));
+
+
+
 const PORT = 4000;
 server.listen(PORT, () => console.log("Server Up And Running On 127.0.0.1:" + PORT));
