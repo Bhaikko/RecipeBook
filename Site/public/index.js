cee12fe8fc944620e346e3e13f40d52dc6fbcad0
Bhaikko <siddhartha.pawar17@gmail.com>
2019-08-25 12:57:57 +0530

    Landing page created

--- Site/public/index.js
+++ Site/public/index.js
@@ -1,11 +1,20 @@
 const parallax = document.getElementById("page1");
+const searchList = document.querySelectorAll("#searchList>li");
+const boxes = document.getElementsByClassName("box");
 
 window.addEventListener("scroll", () => {
     let offset = window.pageYOffset;
     parallax.style.backgroundPositionY = -1 * offset * 0.3 + "px";
 
     if(offset > 100)
+    {
         recipeCircleAnimate();
+        searchListAnimate();
+    }
+
+    if(offset > 800)
+        boxAnimate();
+        
 });
 
 const info2 = document.getElementsByClassName("info2")[0];
@@ -34,3 +43,17 @@ const recipeCircleAnimate = () => {
     });
     
 }
+
+const searchListAnimate = () => {
+    searchList.forEach((list, index) => {
+        list.style.animation = `leftSlide 1s ease forwards ${index + 0.7}s`;
+    }) 
+}
+
+const boxAnimate = () => {
+    boxes[0].style.animation = `leftSlide 1s ease forwards`;
+    boxes[1].style.animation = `rightSlide 1s ease forwards`;
+    
+}
+
+
